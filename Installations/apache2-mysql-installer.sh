58613f14b64278fdec8b0e5ff275dbb5bc16045b
Xcution3r <65634738+Xcution3r@users.noreply.github.com>
2024-10-13 03:31:12 +0300

    Add files via upload

---
+++ Installations/apache2-mysql-installer.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo systemctl status apache2
+sudo apt install mysql-server
+sudo systemctl start mysql
+sudo systemctl enable mysql
+sudo systemctl status mysql
---
+++ Installations/caido-installer.sh
@@ -0,0 +1,4 @@
+wget https://caido.download/releases/v0.42.0/caido-desktop-v0.42.0-linux-x86_64.deb
+sudo dpkg -i https://caido.download/releases/v0.42.0/caido-desktop-v0.42.0-linux-x86_64.deb
+caido
+# sudo chmod 4755 /opt/Caido/chrome-sandbox
